939198ce80e987c7f685e2c41c2f8058ccdff84d
Aaron <aaron@amorgan.net.nz>
2024-11-16 20:28:42 +1300

    2024, quest 1, part 1.

---
+++ src/2024/Quest1.cs
@@ -0,0 +1,27 @@
+﻿using Everybody.Codes.Utilities;
+
+namespace Everybody.Codes._2024;
+
+public class Quest1
+{
+    [Theory]
+    [InlineData("ABBAC", 5)]
+    [InlineData("Quest1.txt", 1328)]
+    public void Day1_Part1_TheBattleForTheFarmlands(string filename, int expectedAnswer)
+    {
+        char[] input = InputParser.ReadAllText("2024/" + filename).ToCharArray();
+        int result = 0;
+        
+        foreach (char c in input)
+        {
+            switch (c)
+            {
+                case 'A': break;
+                case 'B': result += 1; break;
+                case 'C': result += 3; break;
+            }
+        }
+        
+        Assert.Equal(expectedAnswer, result);
+    }
+}
---
+++ src/Utilities/InputParser.cs
@@ -0,0 +1,15 @@
+﻿namespace Everybody.Codes.Utilities;
+
+public static class InputParser
+{
+    /// <summary>
+    /// Returns all lines of the supplied input file as a single string or simply returns the input string if it doesn't end in .txt.
+    /// Indicating it's a test input string.
+    /// </summary>
+    public static string ReadAllText(string filename)
+    {
+        return !filename.EndsWith(".txt")
+            ? filename
+            : File.ReadAllText($"./TestData/{filename}");
+    }
+}
